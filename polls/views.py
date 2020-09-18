2390720924aa90fcb87928acf0039053632a6073
Ed-Games <edmarcosfilho2015@gmail.com>
2020-09-17 23:05:18 -0300

    updated: views only display Questions that have already been published

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from .models import Question, Choice
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -11,7 +12,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class ResultsView(generic.DetailView):
@@ -21,6 +22,13 @@ class ResultsView(generic.DetailView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
